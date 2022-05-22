057b6d0331a820e6823dc6d0a7a484f46253c56d
alexnguyen104 <vstar.alex104@gmail.com>
2022-05-22 17:25:42 +0700

    draw snake

--- snake_game.py
+++ snake_game.py
@@ -11,6 +11,7 @@ game_over = False
 screen = pygame.display.set_mode((s_width, s_height))
 
 GREEN = (0, 255, 0)
+DARK_GREEN = (3, 110, 1)
 RED = (255, 0, 0)
 GRAY = (181, 181, 181)
 
@@ -23,13 +24,22 @@ score = 0
 
 class Snake:
     # First three block of snake's body
-    snake_body = [[0, 0], [25, 0], [50, 0]]
     default_x = 0
     default_y = 0
 
-    def __init__(self, snake_block, snake_speed):
-        self.snake_block = snake_block
-        self.snake_speed = snake_speed
+    def __init__(self, body, block, speed):
+        self.body = body
+        self.block = block
+        self.speed = speed
+        self.direction = "left"
+
+    def draw_snake(self):
+        # Draw head of the snake
+        snake_head = self.body[-1]
+        pygame.draw.rect(screen, DARK_GREEN, [snake_head[0], snake_head[1], self.block, self.block])
+        # Draw body of the snake
+        for i in range(len(self.body) - 1):
+            pygame.draw.rect(screen, GREEN, [self.body[i][0], self.body[i][1], self.block, self.block])
 
 
 class Food:
@@ -39,9 +49,9 @@ class Food:
         pygame.draw.rect(screen, RED, [self.foodx, self.foody, 25, 25])
 
 
-def check_border(x, y):
+def check_border(x, y, block):
     if(x >= 0 and y >= 0):
-        if(x+25 <= s_width and y+25 <= s_height):
+        if(x + block <= s_width and y + block <= s_height):
             return False
     return True
 
@@ -49,7 +59,8 @@ def check_border(x, y):
 count = 0
 
 # Initialize the snake
-snake = Snake(snake_block=25, snake_speed=10)
+block_default = 25
+snake = Snake(body = [[block_default * 3, s_height / 2], [block_default * 4, s_height / 2], [block_default * 5, s_height / 2]],block = block_default, speed = 10)
 # Initialize the food
 food = Food()
 
@@ -79,18 +90,19 @@ while not game_over:
 
     # Draw the map
     for i in range(0, s_width, 50):
-        for j in range(0, int(s_width/snake.snake_block), 2):
+        for j in range(0, int(s_width/snake.block), 2):
             pygame.draw.rect(
-                screen, GRAY, [j*25, i, snake.snake_block, snake.snake_block])
+                screen, GRAY, [j*25, i, snake.block, snake.block])
 
     for i in range(25, s_width, 50):
-        for j in range(1, int(s_width/snake.snake_block), 2):
+        for j in range(1, int(s_width/snake.block), 2):
             pygame.draw.rect(
-                screen, GRAY, [j*25, i, snake.snake_block, snake.snake_block])
+                screen, GRAY, [j*25, i, snake.block, snake.block])
 
-
+    # Draw the snake
+    snake.draw_snake()
 
     pygame.display.update()
-    clock.tick(snake.snake_speed)
+    clock.tick(snake.speed)
 
 pygame.quit()
