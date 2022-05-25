0195b8d291478efaea1b87fa64adae531ba5470c
alexnguyen104 <vstar.alex104@gmail.com>
2022-05-25 20:08:24 +0700

    adding score and food

--- snake_game.py
+++ snake_game.py
@@ -19,26 +19,27 @@ DARK_RED = (255, 174, 165)
 ORANGE = (255, 202, 162)
 PURPLE = (203, 170, 203)
 
-score = 0
-
-
 class Snake:
     def __init__(self, body, block, speed):
         self.body = body
         self.block = block
         self.speed = speed
-        self.direction = None
         self.x = body[-1][0]
         self.y = body[-1][1]
         self.is_stop = True
+        self.direction = None
+        self.score = 0
 
-    def draw_snake(self, new_x, new_y, count):
+    def draw_snake(self, new_x, new_y):
+        # Start to move when the keyboard is pressed for the first time
         if(not self.is_stop):
             self.body.append([new_x, new_y])
             del self.body[0]
+            
         # Draw head of the snake
         snake_head = self.body[-1]
         pygame.draw.rect(screen, DARK_GREEN, [snake_head[0], snake_head[1], self.block, self.block])
+
         # Draw body of the snake
         for i in range(len(self.body) - 1):
             pygame.draw.rect(screen, GREEN, [self.body[i][0], self.body[i][1], self.block, self.block])
@@ -50,30 +51,40 @@ class Snake:
         return True
 
 class Food:
-    def __init__(self, x, y, block):
-        self.x = x
-        self.y = y  
-    def food_providing(self):
-        pygame.draw.rect(screen, RED, [self.x, self.y])
+    def __init__(self, block):
+        self.x = round(random.randrange(0, s_width - block) / block) * block
+        self.y = round(random.randrange(0, s_height - block) / block) * block  
+        self.block = block
 
-
-
-
-
-count = 0
+    def providing(self, snake_body, snake_is_stop):
+        # Check if food is spawned on snake's default position
+        for i in range(len(snake_body)):
+            while(snake_is_stop and [self.x, self.y] == snake_body[i]):
+                self.x = round(random.randrange(0, s_width - self.block) / self.block) * self.block
+                self.y = round(random.randrange(0, s_height - self.block) / self.block) * self.block  
+    
+        pygame.draw.rect(screen, RED, [self.x, self.y, self.block, self.block])
+    
+    def is_eaten(self,snake_x, snake_y):
+        return [self.x, self.y] == [snake_x, snake_y]
+            
+def lose_game(score):
+    # Let the function know that game_over is a global variable
+    global game_over
+    print("You are lose!!!")
+    print("Your score is: " + str(score))
+    game_over = True
 
 # Initialize the snake
 block_default = 25
 snake = Snake(body = [[block_default * 3, s_height / 2], [block_default * 4, s_height / 2], [block_default * 5, s_height / 2]],block = block_default, speed = 10)
 s_x_change = 0
 s_y_change = 0
+
 # Initialize the food
-foodx = round(random.randrange(0, s_width - snake.block) / snake.block) * snake.block
-foody = round(random.randrange(0, s_height - snake.block) / snake.block) * snake.block
-food = Food(foodx, foody)
+food = Food(snake.block)
 
 while not game_over:
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
@@ -101,8 +112,10 @@ while not game_over:
     snake.x += s_x_change
     snake.y += s_y_change
 
+    # Set color of background
     screen.fill(LIGHT_RED)
-    # Draw the map
+
+    # Draw the map's patterns
     for i in range(0, s_width, 50):
         for j in range(0, int(s_width/snake.block), 2):
             pygame.draw.rect(
@@ -114,13 +127,23 @@ while not game_over:
                 screen, DARK_RED, [j*25, i, snake.block, snake.block])
 
     # Draw the snake
-    snake.draw_snake(snake.x, snake.y, count)
+    snake.draw_snake(snake.x, snake.y)
 
+    # Draw the food
+    food.providing(snake.body, snake.is_stop)
+
+    # Check if the snake eats food
+    if food.is_eaten(snake.x, snake.y):
+        snake.score += 1
+        # Set x and y of food again to change its position
+        food.x = round(random.randrange(0, s_width - snake.block) / snake.block) * snake.block
+        food.y = round(random.randrange(0, s_height - snake.block) / snake.block) * snake.block  
+    
     # Check if the snake have touched borders
     if snake.check_border():
-        print("\nLose!")
-        game_over = True
+        lose_game(snake.score)
 
+    # Update changes and set snake's speed
     pygame.display.update()
     clock.tick(snake.speed)
 
